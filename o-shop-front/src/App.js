cb4f8cebc5ff53008e4aa6fd5d69b7ba07d040c8
Loïc-David-Rogeat <desdamo@gmail.com>
2021-01-18 17:24:09 +0100

    Article_Component in progress

--- o-shop-front/src/App.js
+++ o-shop-front/src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import "tailwindcss/tailwind.css";
 
 import Header from './components/Header';
+import ShopArticles from './components/ShopArticles';
 
 function App() {
   return (
     <div className="App">
      < Header />
+     <div className="grid lg:grid-cols-1 xl:grid-cols-2 md:grid-cols-1 ">
+       <div className="bg-red-600 auto-cols-auto	">Menu 
+       <div className="bg-white">
+         <ShopArticles />
+       </div>
+       </div>
+       <div className="bg-yellow-600 auto-cols-min	">2</div>
+       
+     </div>
     </div>
   );
 }
---
+++ o-shop-front/src/components/ShopArticles/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Article from './Article';
+
+const ShopArticles = () => (
+    <div className="flex">
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+    </div>
+    );
+
+
+export default ShopArticles;
