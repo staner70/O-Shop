3f90371cef24cb12ac80817550d82372b31b970c
staner70 <taner2003@gmail.com>
2021-01-21 09:16:32 +0100

    userController

--- o-shop-back/dataMapper/userDataMapper.js
+++ o-shop-back/dataMapper/userDataMapper.js
@@ -9,7 +9,7 @@ userDataMapper = {
     },
 
     // get one user info
-    async getOneUser() {
+    async getOneUser(id) {
         const result = await client.query('SELECT * FROM "user" WHERE id = $1', [id]);
 
         if (result.rowCount == 0 ){
@@ -20,8 +20,9 @@ userDataMapper = {
     },
 
     // create a new user
-    async addOneUser() {
-        const result = await client.query("INSERT INTO user() VALUES RETURNING *", []);
+    async addOneUser(objectUser) {
+        const result = await client.query(`INSERT INTO "user"(username,first_name,last_name,password,role_id) VALUES ($1,$2,$3,$4,$5) RETURNING *`,
+         [objectUser.username,objectUser.first_name,objectUser.last_name, objectUser.password,objectUser.role_id]);
         return result.rows[0];
 
     },
