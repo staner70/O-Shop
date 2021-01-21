21fc2b9012e264dc23653218b5506ad486df6e50
Loïc-David-Rogeat <desdamo@gmail.com>
2021-01-21 09:19:58 +0100

    ajout des categories

--- o-shop-front/src/App.js
+++ o-shop-front/src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import "tailwindcss/tailwind.css";
 import { Route } from 'react-router-dom'
 import  LoginForm from './containers/LoginForm'
+import Home from './components/Home';
 
-import Header from './components/Header';
-import ShopArticles from './components/ShopArticles';
 
 function App() {
   return (
@@ -13,17 +12,7 @@ function App() {
         <LoginForm />
       </Route>
       <Route exact path='/pos'>
-      < Header />
-      <div className="flex">
-        <div className="flex flex-col xl:w-3/5 lg:w-full md:w-full sm:w-full">
-          <div className="bg-red-600">Menu
-       </div>
-          <div className="bg-white">
-            <ShopArticles />
-          </div>
-        </div>
-        <div className="bg-yellow-600 xl:w-2/5 xl:visible	lg:invisible md:invisible sm:invisible">2</div>
-      </div>
+      <Home />
       </Route>
     </div>
   );
---
+++ o-shop-front/src/components/Category/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+const Category = () => (
+    <div className="flex bg-bgred flex-row flex-wrap p-2 w-full">
+        <div className="flex justify-start h-auto w-full overflow-x-auto overflow-y-hidden">
+                <button
+                    className="mx-2 px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                className="px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                    className="mx-2 px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                className="px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                    className="mx-2 px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                className="px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                    className="mx-2 px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+            <button
+                className="px-6 h-10 hover:bg-gray-100 bg-white focus:ring-gray-200 text-gray-900 ring ring-transparent text-xl shadow-md rounded-md uppercase font-semibold focus:outline-none">
+                    Category
+            </button>
+        </div>
+    </div>
+);
+
+
+export default Category;
