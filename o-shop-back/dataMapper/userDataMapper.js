a62bf110a78847e34b6db02fe055a56d357cf669
Anthony Lin <HATOY2Z@gmail.com>
2021-01-21 09:31:26 +0100

    wip dataMapper

--- o-shop-back/dataMapper/userDataMapper.js
+++ o-shop-back/dataMapper/userDataMapper.js
@@ -9,9 +9,9 @@ userDataMapper = {
     },
 
     // get one user info
-    async getOneUser() {
+    async getOneUser(id) {
         const result = await client.query('SELECT * FROM "user" WHERE id = $1', [id]);
-
+        // if the result get nothing return null
         if (result.rowCount == 0 ){
             return null ;
         }
@@ -20,21 +20,31 @@ userDataMapper = {
     },
 
     // create a new user
-    async addOneUser() {
+    async addOneUser(userInfo) {
+        
+        const {username, password } = userInfo;
+        // we test if the user alreayd exist 
+        const existUser = await client.query(`SELECT user.username, user.password FROM "user" WHERE username = $1 AND password = $2`, [userInfo]);
+        // if it exist , return null
+        if (existUser.rowCount != 0){
+            return null
+        }
+        // if not return the result
         const result = await client.query("INSERT INTO user() VALUES RETURNING *", []);
         return result.rows[0];
 
     },
 
-    async updateOneUser() {
-        const result = await client.query(`UPDATE INTO "user" SET username = ,  role_id = WHERE id = $1`,[id]);
+    async updateOneUser(userInfo) {
+        const {first_name, last_name, username, password , role_id, id} = userInfo ;
+
+        const result = await client.query(`UPDATE INTO "user" SET first_name = $1, last_name = $2, username = $4 ,  password = $5, role_id = $6 WHERE id = $7`,[userInfo]);
         return result.rows[0];
-
-
     },
 
-    async DeleteOneUser() {
-        const result = await client.query(``);
+    async DeleteOneUser(userInfo) {
+        const {        } = userInfo ;
+        const result = await client.query(`DELETE * FROM "user" WHERE id = $1`, [userInfo]);
         return result.rows[0];
 
     }
