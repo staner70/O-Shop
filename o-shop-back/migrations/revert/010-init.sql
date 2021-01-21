a01d94633a8ff6e6c403550254f57bff512f747c
kevdanone <kevlevagabon@hotmail.com>
2021-01-21 09:27:59 +0100

    wip login without reload

--- o-shop-back/migrations/revert/010-init.sql
+++
@@ -1,7 +0,0 @@
--- Revert eshop:010-init from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
--- o-shop-back/migrations/verify/010-init.sql
+++
@@ -1,7 +0,0 @@
--- Verify eshop:010-init on pg
-
-BEGIN;
-
--- XXX Add verifications here.
-
-ROLLBACK;
