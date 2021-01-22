4002bd9a785d7feac01f6f9870d5291bebad4108
Loïc-David-Rogeat <desdamo@gmail.com>
2021-01-22 09:10:08 +0100

    ajout component accessforbidden

--- o-shop-front/src/App.js
+++ o-shop-front/src/App.js
@@ -3,6 +3,7 @@ import "tailwindcss/tailwind.css";
 import { Route, Redirect, Switch } from 'react-router-dom';
 import  LoginForm from './containers/LoginForm';
 import Home from './components/Home';
+import AccessForbidden from './components/AccessForbidden';
 
 function App(props) {
   console.log(props);
@@ -14,6 +15,9 @@ function App(props) {
         </Route>
         <Route exact path= '/home'>
           <Home />
+        </Route>
+        <Route exact path= '/accessforbidden'>
+          <AccessForbidden />
         </Route>
       </Switch>
     </div>
