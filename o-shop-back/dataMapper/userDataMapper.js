3068dc83ba96d6ea331428dcf9a08dd564a8e607
Anthony Lin <HATOY2Z@gmail.com>
2021-01-22 16:18:54 +0100

    add a new version of addOneUser

--- o-shop-back/dataMapper/userDataMapper.js
+++ o-shop-back/dataMapper/userDataMapper.js
@@ -21,26 +21,33 @@ userDataMapper = {
 
     // create a new user
     async addOneUser(userInfo) {
-        
+        const {username, first_name, last_name, password, role} = userInfo;
         // we test if the user alreayd exist 
-        const existUser = await client.query(`SELECT username, password FROM "user" WHERE username = $1 AND password = $2`, [userInfo.username, userInfo.password ]);
+        const existUser = await client.query(`SELECT username, password FROM "user" WHERE username = $1 AND password = $2`, [username, password ]);
         // if it exist , return null
         if (existUser.rowCount != 0){
             return null;
         }
+        // add role name for the user based on the role_id
+            //first we get the id
+            console.log(role, 'role');
+        const roleId = await client.query(`SELECT id FROM role WHERE name = $1`, [role]);
+        
         // if not return the result
         const result = await client.query(`INSERT INTO "user"(username,first_name,last_name,password,role_id) VALUES ($1,$2,$3,$4,$5) RETURNING *`,
-        [userInfo.username,userInfo.first_name,userInfo.last_name, userInfo.password,userInfo.role_id]);
+        [username, first_name, last_name, password, roleId.rows[0].id]);
         return result.rows[0];
 
     },
 
     //update a user
     async updateOneUser(id, userInfo) {
-        const {first_name, last_name, username, password , role_id} = userInfo ;
+        const {first_name, last_name, username, password , role} = userInfo ;
+
+        const roleId = await client.query(`SELECT id FROM role WHERE name = $1`, [role]);
 
         const result = await client.query(`UPDATE "user" SET first_name = $1, last_name = $2, username = $3 ,  password = $4, role_id = $5 WHERE id = $6 RETURNING *`,
-        [first_name, last_name, username, password , role_id, id]);
+        [first_name, last_name, username, password ,roleId.rows[0].id, id]);
         
         // if there is no user return null
         if (result.rowCount == 0) {
