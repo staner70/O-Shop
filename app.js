39bb2165b98980b66bf0b1168d9eef62ba4dd92f
staner70 <taner2003@gmail.com>
2021-01-27 18:32:56 +0100

    package multer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const router = require('./o-shop-back/routers');
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors("*"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'o-shop-back/public')));
 app.use(express.urlencoded({ extended: true }))
 app.use(router);
 
