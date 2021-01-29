394328466cab4eae4428307bddcf40da8e4df17d
Anthony Lin <HATOY2Z@gmail.com>
2021-01-29 16:26:11 +0100

    add Joi

---
+++ o-shop-back/helpers/validation/validationMiddleware.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    validateBody(schema) {
+        return async (request, response, next) => {
+            try {
+                const validation = await schema.validateAsync(request.body);
+
+                // validation contient un propriété error si qqc c'est mal passé
+                if (validation.error) {
+                    // Réponse d'erreur
+                    response.status(400).json({error: validation.error});
+                    return;
+                }
+                next();
+
+            } catch (error) {
+                if (error.message == 'category.invalid (category)') {
+                    response.status(400).json({error: {
+                        details: error.message,
+                        message: "Le category name n'existe pas"
+                    }});
+                    return;
+                }
+                if (error.message == 'shop.invalid (shop)') {
+                    response.status(400).json({error: {
+                        details: error.message,
+                        message: "Le shop name n'existe pas"
+                    }});
+                    return;
+                }
+                if (error.message == 'role.invalid (role)') {
+                    response.status(400).json({error: {
+                        details: error.message,
+                        message: "Le role name n'existe pas"
+                    }});
+                    return;
+                }
+                next(error);
+            }
+        };
+    },
+};
--- o-shop-back/routers/user.js
+++ o-shop-back/routers/user.js
@@ -13,12 +13,14 @@ const {
 const { catchErrors } = require('../helpers/catchError');
 const { getAccessToRoute, getAdminAccess } = require('../middlewares/authorization/auth');
 const { checkUserExist } = require('../middlewares/database/databaseErrorHelpers');
+const { validateBody } = require('../helpers/validation/validationMiddleware');
+const { userSchema } = require('../helpers/validation/schema');
 
 router.get('/', getAccessToRoute, catchErrors(getAllUser));
 router.get('/:id(\\d+)', getAccessToRoute, catchErrors(checkUserExist),  catchErrors(getOneUser));
 
-router.post('/', getAccessToRoute, getAdminAccess, catchErrors(addOneUser));
-router.patch('/:id(\\d+)', getAccessToRoute, getAdminAccess, catchErrors(updateOneUser));
+router.post('/', getAccessToRoute, getAdminAccess, validateBody(userSchema),  catchErrors(addOneUser));
+router.patch('/:id(\\d+)', getAccessToRoute, getAdminAccess, validateBody(userSchema), catchErrors(updateOneUser));
 router.delete('/:id(\\d+)', getAccessToRoute, getAdminAccess, catchErrors(deleteOneUser));
 
 router.patch('/:userId/shop/:shopId',  getAccessToRoute, getAdminAccess, catchErrors(associateWork));
