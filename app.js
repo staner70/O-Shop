7f0762e60d53251128dbbd2984ae88166ecff0d6
staner70 <taner2003@gmail.com>
2021-02-04 21:04:58 +0100

    socket broadcast

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const app = express();
 app.use(cors("*"));
 
 // create pipeline socket.io
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'o-shop-back/public')));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(router);
 
-// app.set('socketio', io);
-
+io.on('connection', (socket) => {
+   console.log('Client connected');
+   app.set('socketio', io);
+   socket.on('disconnect', () => console.log('Client disconnected'));
+ });
 
 http.listen(process.env.PORT || 3500, () => {
    console.log('Server running on :', process.env.PORT);
