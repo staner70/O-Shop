5fbbc1e6cf5bc1daa39d683127b876a38363f61a
staner70 <taner2003@gmail.com>
2021-02-05 09:39:51 +0100

    correction socket cors

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const path = require('path');
 const router = require('./o-shop-back/routers');
 
 const app = express();
-app.use(cors("*"));
+app.use(cors({
+   "origin": "*",
+   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+   "preflightContinue": false,
+   "optionsSuccessStatus": 204
+ }));
 
 
 
